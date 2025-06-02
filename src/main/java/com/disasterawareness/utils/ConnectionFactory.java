48c2768454a7ced4b4c377861206bdaae0daeca3
bitoller <bi_toller@hotmail.com>
2025-06-02 16:42:46 -0300

    fix: Update database credentials in ConnectionFactory

--- src/main/java/com/disasterawareness/utils/ConnectionFactory.java
+++ src/main/java/com/disasterawareness/utils/ConnectionFactory.java
@@ -8,24 +8,24 @@ import java.util.concurrent.TimeUnit;
 
 public class ConnectionFactory {
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    private static final String USER = "rm553134";
-    private static final String PASSWORD = "060892";
-    
+    private static final String USER = "rm552726";
+    private static final String PASSWORD = "120289";
+
     static {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
+            System.out.println("Conexão realizada!");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Erro ao carregar o driver do Oracle: " + e.getMessage());
         }
     }
-    
+
     public static Connection getConnection() throws SQLException {
         Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
         connection.setNetworkTimeout(Executors.newSingleThreadExecutor(), (int) TimeUnit.MINUTES.toMillis(5));
         return connection;
     }
-    
+
     private ConnectionFactory() {
-        // Construtor privado para evitar instanciação
     }
-} 
+}
